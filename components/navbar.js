470063d9099583e422021a2678138473ee2dde68
Preeti Sharma <preetbhardwaj517@gmail.com>
2022-02-23 10:43:10 +0530

    homepage
    navbar

---
+++ components/navbar.js
@@ -0,0 +1,21 @@
+            function navbar(){
+
+
+                return `
+               
+         <div>Z A R A   H O M E</div>
+         <div>
+            <label for="">SEARCH</label>
+            <a href="search.html">
+                  <input type="text"></a>
+         </div>
+         <div id="lst">
+            <a href="">LOG IN</a>
+            <a href="">cart</a>
+
+         </div>
+    
+                
+                `;
+            }
+            export default navbar
