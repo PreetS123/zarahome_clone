069fb2474acae23975e7404cb82b364ef3345bd7
Preeti Sharma <preetbhardwaj517@gmail.com>
2022-02-25 11:27:34 +0530

    home_page

---
+++ components/footer.js
@@ -0,0 +1,60 @@
+
+function footerZara() {
+
+    return `
+    
+    <div id="footer_1st">
+    <input type="text"  placeholder="JOIN OUR NEWSLETTER - ENTER YOUR EMAIL ADDRESS" required>
+     
+</div>
+<div id="footer_2nd">
+    <div id="follow">
+        <p>Follow us on </p>
+        <div class="icon"> 
+        <a href="https://www.facebook.com/zarahome"><i class="fab fa-facebook-f"></i> </a>
+        <a href="https://www.instagram.com/zarahome/"> <i class="fab fa-instagram"></i></a>
+        <a href="https://www.pinterest.es/zarahome/"> <i class="fab fa-pinterest-p"></i>  </a>
+        <a href="https://www.youtube.com/channel/UCrBndgD3vCicJ0nLRoKCCaA">  <i class="fa fa-youtube"></i> </a>
+        <a href="https://twitter.com/zarahome"> <i class="fab fa-twitter"></i>  </a>
+        <a href="https://open.spotify.com/user/fbune3k41mta54xtdc3d8v7xu?si=FQQ6HDU7RZWT_yqDIVYAmw&nd=1">  <i class="fa fa-spotify"></i>  </a> 
+         
+        
+        
+       
+        </div>
+    </div>
+    <div id="download">
+        <p>Download our app</p>
+      
+        <div class="icon">
+             <a href="https://apps.apple.com/es/app/zarahome-shop-online/id474175190"><i class="fa fa-apple"> </i></a>
+             <a href="">   <i class="fa fa-apple"> </i></a>
+           
+        </div>
+    </div>
+</div>
+<div id="footer_3rd">
+    <div>
+        <h5>POLICIES</h5>
+        <a href="#"><p>TERMS AND CONDITIONS</p></a>
+        <a href="#"><p>PRIVACY POLICY</p></a>
+        <a href="#"><p>COOKIES POLICY</p></a>
+        <a href="#"><p>COOKIES SETTINGS</p></a>
+        <a href="#"><p>LEGAL MENTIONS
+        </p></a>
+    </div>
+    <div>
+        <h5>COMPANY</h5>
+        <a href="#"><p>WORK WITH US</p></a>
+        <a href="#"><p>PRESS</p></a>
+    </div>
+    <div>
+        <h5>CONTACT</h5>
+        <a href="#"><p>CONTACT US</p></a>
+        <a href="#"><p>GUEST PURCHASE</p></a>
+    </div>
+</div>
+    `;
+}
+export default footerZara
+
--- components/navbar.js
+++ components/navbar.js
@@ -2,19 +2,54 @@
 
 
                 return `
-               
-         <div>Z A R A   H O M E</div>
-         <div>
-            <label for="">SEARCH</label>
-            <a href="search.html">
-                  <input type="text"></a>
-         </div>
-         <div id="lst">
-            <a href="">LOG IN</a>
-            <a href="">cart</a>
+                <div id="container_nav">
+    <div id="zara-home">
+      <div class="flex">
+        <i class="fa fa-bars"></i>
+        <!-- zara home button -->
+        <button class="button " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling"
+          aria-controls="offcanvasScrolling">
+          <img id="logo" src="https://www.zarahome.com/static/itxwebstandard/images/logo.svg" alt="">
+        </button>
+      </div>
 
-         </div>
-    
+      <div id="search-box">
+        <label for="">SEARCH</label>
+        <input type="search">
+      </div>
+      <div id="right">
+        <div>
+          <i class="fa fa-user"></i>
+          LOGIN</div>
+         
+        <div id="cart_logo_img" class="material-icons"> shopping_bag 
+       </div>
+         
+      </div>
+    </div>
+  
+<!-- sidebar -->
+    <div  class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1"
+      id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+      <div class="offcanvas-header">
+        <img id="logo"
+          src="https://www.zarahome.com/static/itxwebstandard/images/logo.svg" alt="">
+          <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+
+       
+        <div> <a href="">NEW IP </a></div>
+        <div> <a href="">BEDROOM </a></div>
+        <div> <a href="">CLOTHING & FOOTWEAR </a></div>
+        <div> <a href="">LIVING ROOM </a></div>
+        <div> <a href="">DINING </a></div>
+        <div> <a href="">BATHROOM </a></div>
+        <div> <a href="">KIDS </a></div>
+        <div> <a href="">STORIES </a></div>
+
+    </div>
+
+     </div>
                 
                 `;
             }
