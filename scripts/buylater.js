815bf83ef9713890d3f49850a1d59cd70f38ea45
adarshakhatua <97473442+adarshakhatua@users.noreply.github.com>
2022-02-27 20:26:13 +0530

    Update buylater.js

--- scripts/buylater.js
+++ scripts/buylater.js
@@ -1,181 +1,112 @@
-
-
-
-function populateInbuylaterPage(){
-
-    var buylater=JSON.parse(localStorage.getItem("buylaterArray"))||[]
-    document.getElementById("items").innerHTML=""
-    buylater.map((elem,index,array)=>{
-
-        var card=document.createElement("div")
-    card.className="card"
-
-    var image=document.createElement("img");
-    image.src=elem.imagelink1
-
-    var card=document.createElement("div");
-    card.className="card"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="/style/buylater.css">
 
-    var fav_div=document.createElement("div");
-    fav_div.className="fav-div"
+    <title>Buy Later</title>
+</head>
+<body>
+    <div id="container">
+        <div id="top">
+            <div id="SHOPPING-BASKET">SHOPPING BASKET (12)</div>
+            <div id="BUY-LATER">BUY LATER (0)</div>
+        </div>
+        
+        <div id="content">
+            <div id="items">
+                <div class="card">
+                    <img src="https://static.zarahome.net/8/photos4/2022/V/1/1/p/1021/800/002/1021800002_1_1_5.jpg?t=1627308169361" alt="">
+                    <div class="fav-div">
+                        <div class="fab">
+                            <div class="buy-later-btn">BUY LATER</div>
+                            <div class="material-icons bag-icon">shopping_bag</div>
+                            <div class="material-icons cross-icon">close</div>
+                        </div>
+                    </div>
 
-    var fav=document.createElement("fav");
-    fav.className="fab"
+                    <div class="txt">
+                        <div id="name">FAUX FUR SLIPPERS</div>
+                    <div id="price-div">
+                        <div id="price">25.99 € </div>
+                        <div id="quantity">
+                            <div id="minus">-</div>
+                            <div id="count">1</div>
+                            <div id="plus">+</div>
+                        </div>
+                    </div>
 
-    var buy_later_btn=document.createElement("div");
-    buy_later_btn.className="buy-later-btn"
-    buy_later_btn
+                    <div id="ref1">Ref. 1021/800/002/40</div>
+                    <div id="ref2">40</div>
+                    </div>
 
-    var fab_icon=document.createElement("div");
-    fab_icon.className="material-icons bag-icon"
-    fab_icon.textContent="shopping_bag"
-    fab_icon.addEventListener("click",()=>{addToBasket(elem,index)})
+                </div>
 
-    var cross_icon=document.createElement("div");
-    cross_icon.className="material-icons cross-icon";
-    cross_icon.textContent="close"
-    cross_icon.addEventListener("click",()=>{removeItems(index)})
+                <div class="card">
+                    <img src="https://static.zarahome.net/8/photos4/2022/V/1/1/p/1021/800/002/1021800002_1_1_5.jpg?t=1627308169361" alt="">
+                    <div class="fav-div">
+                        <div class="fab">
+                            <div class="buy-later-btn">BUY LATER</div>
+                            <div class="material-icons bag-icon">shopping_bag</div>
+                            <div class=" material-icons cross-icon">close</div>
+                        </div>
+                    </div>
 
-    var txt=document.createElement("div");
-    txt.className="txt";
+                    <div class="txt">
 
-    var name=document.createElement("div");
-    name.id="name";
-    name.textContent=elem.name;
+                        <div id="name">FAUX FUR SLIPPERS</div>
+                    <div id="price-div">
+                        <div id="price">25.99 € </div>
+                        <div id="quantity">
+                            <div id="minus">-</div>
+                            <div id="count">1</div>
+                            <div id="plus">+</div>
+                        </div>
+                    </div>
 
-    var price_div=document.createElement("div")
-    price_div.id="price-div";
+                    <div id="ref1">Ref. 1021/800/002/40</div>
+                    <div id="ref2">40</div>
+                    </div>
 
-    var price=document.createElement("div")
-    price.id="price";
-    price.textContent=elem.price;
+                </div>
 
-    var quantity=document.createElement("div")
-    quantity.id="quantity";
+                <div class="card">
+                    <img src="https://static.zarahome.net/8/photos4/2022/V/1/1/p/1021/800/002/1021800002_1_1_5.jpg?t=1627308169361" alt="">
+                    <div class="fav-div">
+                        <div class="fab">
+                            <div class="buy-later-btn">BUY LATER</div>
+                            <div class="material-icons bag-icon">shopping_bag</div>
+                            <div class="material-icons cross-icon">close</div>
+                        </div>
+                    </div>
 
-    var minus=document.createElement("div")
-    minus.id="minus";
-    minus.textContent="-"
-    minus.addEventListener("click",()=>{decreaseQty(elem,array)})
+                    <div class="txt">
+                        <div id="name">FAUX FUR SLIPPERS</div>
+                    <div id="price-div">
+                        <div id="price">25.99 € </div>
+                        <div id="quantity">
+                            <div id="minus">-</div>
+                            <div id="count">1</div>
+                            <div id="plus">+</div>
+                        </div>
+                    </div>
 
-    var count=document.createElement("div")
-    count.id="count";
-    count.textContent=elem.qty;
+                    <div id="ref1">Ref. 1021/800/002/40</div>
+                    <div id="ref2">40</div>
+                    </div>
 
-    var plus=document.createElement("div")
-    plus.id="plus";
-    plus.textContent="+"
-    plus.addEventListener("click",()=>{increaseQty(elem,array)})
+                </div>
 
-    var ref1=document.createElement("div")
-    ref1.id="ref1";
-    ref1.textContent=elem.ref;
-    
-    var ref2=document.createElement("div")
-    ref2.id="ref2";
+            </div>
 
+            
+        </div>
+    </div>
+</body>
+</html>
 
-
-
-    fav.append(fab_icon,cross_icon);
-    fav_div.append(fav);
-
-    price_div.append(price,quantity);
-    quantity.append(minus,count,plus);
-    txt.append(name,price_div,ref1,ref2);
-
-    card.append(image,fav_div,txt);
-
-    document.getElementById("items").append(card);
-    })
-
-
-    buylaterCounter()
-}
-
-populateInbuylaterPage()
-
-
-
-function removeItems(index){
-    let buylater=JSON.parse(localStorage.getItem("buylaterArray")) ||[];
-    buylater.splice(index,1);
-    localStorage.setItem("buylaterArray",JSON.stringify(buylater))
-    populateInbuylaterPage()
-    buylaterCounter()
-    emptybuylater()
-}
-
-
-function addToBasket(elem,index){
-    var cart=JSON.parse(localStorage.getItem("basketArray")) ||[];
-    cart.push(elem);
-    localStorage.setItem("basketArray",JSON.stringify(cart))
-    let buylater=JSON.parse(localStorage.getItem("buylaterArray"));
-    buylater.splice(index,1);
-    localStorage.setItem("buylaterArray",JSON.stringify(buylater))
-    populateInbuylaterPage()
-    buylaterCounter()
-    cartCount()
-    emptybuylater()
-}
-
-function cartCount(){
-    let cart=JSON.parse(localStorage.getItem("basketArray"))||[];
-    let count=0;
-    for(let i of cart){
-        count+=(i.qty)
-    }
-    document.getElementById("SHOPPING-BASKET").textContent=`SHOPPING BASKET (${count})`
-}
-cartCount()
-
-
-function buylaterCounter(){
-    let buylater=JSON.parse(localStorage.getItem("buylaterArray")) ||[];
-    let count=0;
-    for(let i of buylater){
-        count+=(i.qty)
-    }
-    document.getElementById("BUY-LATER").textContent=`BUY LATER (${count})`
-}
-
-
-document.getElementById("SHOPPING-BASKET").addEventListener("click",()=>{window.location.href="/cart page/cart.html"})
-
-
-export {buylaterCounter};
-
-
-function decreaseQty(elem,array){
-    if(elem.qty!==1){
-        elem.qty--;
-    }
-    localStorage.setItem("buylaterArray",JSON.stringify(array))
-    populateInbuylaterPage();
-    cartCount()
-}
-function increaseQty(elem,array){
-        elem.qty++;
-    localStorage.setItem("buylaterArray",JSON.stringify(array))
-    populateInbuylaterPage();
-    cartCount()
-}
-
-
-function emptybuylater(){
-    let buylater=JSON.parse(localStorage.getItem("buylaterArray")) ||[];
-    if(buylater.length==0){
-        let empty_card=document.createElement("div")
-        empty_card.id="empty-card"
-        empty_card.textContent="Your list of items saved for later is empty."
-
-        document.getElementById("items").append(empty_card)
-    }
-}
-emptybuylater()
-
-
-
-
+<script src="/scripts/buylater.js" type="module"></script>
