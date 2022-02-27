042d6a9c982162e6aa2cf8bb6303f21d06c32ce3
adarshakhatua <97473442+adarshakhatua@users.noreply.github.com>
2022-02-27 20:26:50 +0530

    Update cart.js

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,216 +1,112 @@
-
-
-
-function populateInCartPage(){
-    let cart=JSON.parse(localStorage.getItem("basketArray")) ||[];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    document.getElementById("items").innerHTML=""
-    cart.map((elem,index,array)=>{
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="/style/buylater.css">
 
-        var image=document.createElement("img");
-        image.src=elem.imagelink1
-
-        var card=document.createElement("div");
-        card.className="card"
+    <title>Buy Later</title>
+</head>
+<body>
+    <div id="container">
+        <div id="top">
+            <div id="SHOPPING-BASKET">SHOPPING BASKET (12)</div>
+            <div id="BUY-LATER">BUY LATER (0)</div>
+        </div>
         
+        <div id="content">
+            <div id="items">
+                <div class="card">
+                    <img src="https://static.zarahome.net/8/photos4/2022/V/1/1/p/1021/800/002/1021800002_1_1_5.jpg?t=1627308169361" alt="">
+                    <div class="fav-div">
+                        <div class="fab">
+                            <div class="buy-later-btn">BUY LATER</div>
+                            <div class="material-icons bag-icon">shopping_bag</div>
+                            <div class="material-icons cross-icon">close</div>
+                        </div>
+                    </div>
 
-        var fav_div=document.createElement("div");
-        fav_div.className="fav-div"
+                    <div class="txt">
+                        <div id="name">FAUX FUR SLIPPERS</div>
+                    <div id="price-div">
+                        <div id="price">25.99 € </div>
+                        <div id="quantity">
+                            <div id="minus">-</div>
+                            <div id="count">1</div>
+                            <div id="plus">+</div>
+                        </div>
+                    </div>
 
-        var fav=document.createElement("fav");
-        fav.className="fab"
+                    <div id="ref1">Ref. 1021/800/002/40</div>
+                    <div id="ref2">40</div>
+                    </div>
 
-        var buy_later_btn=document.createElement("div");
-        buy_later_btn.className="buy-later-btn"
-        buy_later_btn.textContent="BUY LATER"
-        buy_later_btn.addEventListener("click",()=>{addToBuylater(elem,index)})
+                </div>
 
-        var fab_icon=document.createElement("div");
-        fab_icon.className="material-icons fab-icon";
-        fab_icon.textContent=colorWishlisticon(elem);
-        fab_icon.addEventListener("click",(e)=>{addToWishlist(e,elem)});
+                <div class="card">
+                    <img src="https://static.zarahome.net/8/photos4/2022/V/1/1/p/1021/800/002/1021800002_1_1_5.jpg?t=1627308169361" alt="">
+                    <div class="fav-div">
+                        <div class="fab">
+                            <div class="buy-later-btn">BUY LATER</div>
+                            <div class="material-icons bag-icon">shopping_bag</div>
+                            <div class=" material-icons cross-icon">close</div>
+                        </div>
+                    </div>
 
-        var cross_icon=document.createElement("div");
-        cross_icon.className="material-icons cross-icon";
-        cross_icon.textContent="close"
-        cross_icon.addEventListener("click",()=>{removeItems(index)})
+                    <div class="txt">
 
-        var txt=document.createElement("div");
-        txt.className="txt";
+                        <div id="name">FAUX FUR SLIPPERS</div>
+                    <div id="price-div">
+                        <div id="price">25.99 € </div>
+                        <div id="quantity">
+                            <div id="minus">-</div>
+                            <div id="count">1</div>
+                            <div id="plus">+</div>
+                        </div>
+                    </div>
 
-        var name=document.createElement("div");
-        name.className="name";
-        name.textContent=elem.name;
+                    <div id="ref1">Ref. 1021/800/002/40</div>
+                    <div id="ref2">40</div>
+                    </div>
 
-        var price_div=document.createElement("div")
-        price_div.className="price-div";
+                </div>
 
-        var price=document.createElement("div")
-        price.className="price";
-        price.textContent=elem.price;
+                <div class="card">
+                    <img src="https://static.zarahome.net/8/photos4/2022/V/1/1/p/1021/800/002/1021800002_1_1_5.jpg?t=1627308169361" alt="">
+                    <div class="fav-div">
+                        <div class="fab">
+                            <div class="buy-later-btn">BUY LATER</div>
+                            <div class="material-icons bag-icon">shopping_bag</div>
+                            <div class="material-icons cross-icon">close</div>
+                        </div>
+                    </div>
 
-        var quantity=document.createElement("div")
-        quantity.className="quantity";
+                    <div class="txt">
+                        <div id="name">FAUX FUR SLIPPERS</div>
+                    <div id="price-div">
+                        <div id="price">25.99 € </div>
+                        <div id="quantity">
+                            <div id="minus">-</div>
+                            <div id="count">1</div>
+                            <div id="plus">+</div>
+                        </div>
+                    </div>
 
-        var minus=document.createElement("div")
-        minus.className="minus";
-        minus.textContent="-"
-        minus.addEventListener("click",(e)=>{decreaseQty(elem,array,e)})
+                    <div id="ref1">Ref. 1021/800/002/40</div>
+                    <div id="ref2">40</div>
+                    </div>
 
-        var count=document.createElement("div")
-        count.className="count";
-        count.textContent=elem.qty
+                </div>
 
-        var plus=document.createElement("div")
-        plus.className="plus";
-        plus.textContent="+"
-        plus.addEventListener("click",(e)=>{increaseQty(elem,array,e)})
+            </div>
 
-        var ref1=document.createElement("div")
-        ref1.className="ref1";
-        ref1.textContent=elem.ref;
-        
-        var ref2=document.createElement("div")
-        ref2.className="ref2";
+            
+        </div>
+    </div>
+</body>
+</html>
 
-
-
-
-        fav.append(buy_later_btn,fab_icon,cross_icon);
-        fav_div.append(fav);
-
-        price_div.append(price,quantity);
-        quantity.append(minus,count,plus);
-        txt.append(name,price_div,ref1,ref2);
-
-        card.append(image,fav_div,txt);
-
-        document.getElementById("items").append(card);
-        
-    })
-    document.getElementById("empty_basket_btn").style.display="block"
-    
-    
-}
-populateInCartPage()
-
-
-function removeItems(index){
-    let cart=JSON.parse(localStorage.getItem("basketArray")) ||[];
-    cart.splice(index,1);
-    localStorage.setItem("basketArray",JSON.stringify(cart))
-    populateInCartPage()
-    cartCounter()
-    emptycart()
-}
-
-import { buylaterCounter } from "/scripts/buylater.js";
-function addToBuylater(elem,index){
-    var buylater=JSON.parse(localStorage.getItem("buylaterArray")) || [];
-    buylater.push(elem);
-    localStorage.setItem("buylaterArray",JSON.stringify(buylater))
-
-    let cart=JSON.parse(localStorage.getItem("basketArray"));
-    cart.splice(index,1);
-    localStorage.setItem("basketArray",JSON.stringify(cart))
-    populateInCartPage()
-    cartCounter()
-    buylaterCounter()
-    emptycart()
-}
-
-function decreaseQty(elem,array){
-    if(elem.qty!==1){
-        elem.qty--;
-    }
-    
-    localStorage.setItem("basketArray",JSON.stringify(array))
-    populateInCartPage()
-    cartCounter()
-}
-function increaseQty(elem,array,e){
-    elem.qty++;
-    localStorage.setItem("basketArray",JSON.stringify(array));
-    populateInCartPage();
-    cartCounter();
-}
-
-
-function cartCounter(){
-    let cart=JSON.parse(localStorage.getItem("basketArray")) ||[];
-    var count=0;
-    var price=0
-    for(let i of cart){
-
-        count+=(i.qty)
-        price+=i.qty*i.price.slice(0,-2)
-    }
-    price=(price.toFixed(2));
-    document.getElementById("SHOPPING-BASKET").textContent=`SHOPPING BASKET (${count})`
-    document.getElementById("total-items").textContent=`${count} ITEMS`
-    document.getElementById("total-price-including-tax-data").textContent=`${price} €`
-    document.getElementById("total-price-data").textContent=`${price} €`
-   
-}
-
-cartCounter()
-
-
-document.getElementById("BUY-LATER").addEventListener("click",()=>{window.location.href="/cart page/buylater.html"})
-
-
-
-function emptycart(){
-    let cart=JSON.parse(localStorage.getItem("basketArray")) ||[];
-    if(cart.length==0){
-        document.getElementById("items").innerHTML="";
-        let empty_cart=document.createElement("div");
-        empty_cart.id="empty-card"
-
-        let image=document.createElement("img");
-        image.id="empty-image";
-        image.src="https://symbols-electrical.getvecta.com/stencil_265/34_heat-exchanger-steam-boiler.4aab0680bb.svg";
-
-        let empty_txt=document.createElement("div");
-        empty_txt.textContent="Your shopping basket is empty";
-        empty_txt.id="empty-txt";
-
-
-        empty_cart.append(image,empty_txt)
-        document.getElementById("items").append(empty_cart)
-        document.getElementById("cart-details").style.display="none"
-        document.getElementById("empty_basket_btn").style.display="none"
-    }
-}
-emptycart()
-
-
-//add to wishlist...
-
-function addToWishlist(e,elem){
-   let wishlist=JSON.parse(localStorage.getItem("wish_list")) || [];
-
-   var isInWishlist=false;
-   for(let i=0; i<wishlist.length; i++){
-     if(elem.ref==wishlist[i].ref){wishlist.splice(i,1); e.target.textContent="favorite_border"; isInWishlist=true;}
-   }
-   if(isInWishlist==false){e.target.textContent="favorite"; wishlist.push(elem);}
-
-   localStorage.setItem("wish_list",JSON.stringify(wishlist));
-}
-
-//add to wishlist...
-
-//wishlist icon...
-function colorWishlisticon(elem){
-    let wishlist=JSON.parse(localStorage.getItem("wish_list")) || [];
-        for(let i=0; i<wishlist.length; i++){
-            if(elem.ref==wishlist[i].ref){return"favorite";}
-        }
-        return "favorite_border";
-}
-//wishlist icon...
-
-
-
-document.getElementById("empty_basket_btn").addEventListener("click",()=>{localStorage.setItem("basketArray",JSON.stringify([]));emptycart();cartCounter()})
+<script src="/scripts/buylater.js" type="module"></script>
